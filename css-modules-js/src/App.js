663970f7ed286befdb790b95317073bea0c5ee03
Paul Laskowski <paul@warmbowski.org>
2018-02-11 17:00:44 -0800

    refactor style classNames for css-modules

--- css-modules-js/src/App.js
+++ css-modules-js/src/App.js
@@ -5,12 +5,12 @@ import styles from './App.css';
 class App extends Component {
   render() {
     return (
-      <div className={styles['App']}>
-        <header className={styles['App-header']}>
-          <img src={logo} className={styles['App-logo']} alt="logo" />
-          <h1 className={styles['App-title']}>Welcome to React</h1>
+      <div className={styles.section}>
+        <header className={styles.header}>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <h1 className={styles.title}>Welcome to React</h1>
         </header>
-        <p className={styles['App-intro']}>
+        <p className={styles.intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
